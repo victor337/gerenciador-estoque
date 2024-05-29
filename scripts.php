1de8c3621d26eb1a880e371814a9f60db82a2ac0
Victor Hugo <victor.hugo@external.fcagroup.com>
2024-05-29 19:11:41 -0300

    Estruturas iniciais

---
+++ scripts.php
@@ -0,0 +1,20 @@
+<?php
+
+function saveProduct($arg_1,)
+{
+    //'SELECT * FROM prudcts where email = '$email'';
+    return true;
+}
+
+function editProduct($arg_1,)
+{
+    //'SELECT * FROM prudcts where email = '$email'';
+    return true;
+}
+
+function login($arg_1,)
+{
+    //'SELECT * FROM prudcts where email = '$email'';
+    return true;
+}
+?>
